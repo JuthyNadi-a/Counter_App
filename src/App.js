71a6b6de32b269436727c2d6819caf315d2c9e2e
JuthyNadi-a <juthynadia@gmail.com>
2022-10-26 13:41:19 +0300

    color change feature added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
-
-
 const App = () => {
   return (
     <div>
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,6 @@
 import './Main.css'
 import React, { Component } from "react";
+import Button from "./Button"
 
 class Main extends Component {
     state= {
@@ -15,11 +16,11 @@ class Main extends Component {
         this.setState({counter: 0})
     };
     resetOneHandler = () => { 
-        if (this.state.counter <= 5) 
+        if (this.state.counter <= 0) 
             {
                 this.setState({counter: 0})
             }
-        else if (this.state.counter > 0) 
+        else 
         {
             this.setState({counter: this.state.counter - 1})
         }
@@ -29,22 +30,36 @@ class Main extends Component {
             {
                 this.setState({counter: 0})
             }
-        else if (this.state.counter > 0)
+        else 
             {
             this.setState({counter: this.state.counter - 5})
             } 
     };
     render() {
+        let circleClass = "";
+        if (this.state.counter === 0){
+            circleClass = "circle";
+        }
+        else if (this.state.counter%2 === 0){
+            circleClass = "circle-even";
+        } else {
+            circleClass = "circle-odd";
+        }
         return (
             <main>
-                <div>
-                <div className='header'><h1>{this.state.counter}</h1></div>
+                <div className='content'>
+                <h1 className={circleClass} id='header'>{this.state.counter}</h1>
                     <div className='buttons'> 
-                    <button onClick={this.addFiveHandler}>Add five</button>
+                    <Button click = {this.addFiveHandler}>Add five</Button>
+                    <Button click = {this.addOneHandler}>Add one</Button>
+                    <Button click = {this.resetHandler}>Reset</Button>
+                    <Button click = {this.resetOneHandler}>Reset one</Button>
+                    <Button click = {this.resetFiveHandler}>Reset five</Button>
+                    {/* <button onClick={this.addFiveHandler}>Add five</button>
                     <button onClick={this.addOneHandler}>Add one</button>
                     <button onClick={this.resetHandler}>Reset</button>
                     <button onClick={this.resetOneHandler}>Reset one</button>
-                    <button onClick={this.resetFiveHandler}>Reset five</button>
+                    <button onClick={this.resetFiveHandler}>Reset five</button> */}
                     </div>
                 </div>
             </main>
